8fd6d66f2139db803f5b98ed6c6c055bca993423
Vikash saini <vikashsaini9789@gmail.com>
2025-08-01 14:59:59 +0530

    repeatingfunction

---
+++ repeatingnumbers.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+arr = [1, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 3, 2, 5, 3, 3, 5]
+
+count = Counter(arr)
+
+repeats = [item for item in count.items() if item[1] > 1]
+
+repeats.sort(key=lambda x: -x[1])
+
+for i in range(len(repeats)):
+    print(f"most repeating number : {repeats[i][0]} ({repeats[i][1]} times)")
---
+++ thirdelementrepeating.py
@@ -0,0 +1,5 @@
+arr = [1, 2, 3, 4, 3, 5, 3, 4, 1, 2, 4, 3]
+third_element = arr[2]
+count = arr.count(third_element)
+
+print(f"the third element ({third_element}) appears {count} times.")
